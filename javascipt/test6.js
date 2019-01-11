2c503e8482c234e572e11d4fceaeec95c5342877
Vladyslav Ganushkevych <ganushkevychv@gmail.com>
2019-01-11 21:17:37 +0200

    learned how to deal with FOR Loops in JavaScript

---
+++ javascipt/test6.js
@@ -0,0 +1,8 @@
+var links = ducument.getElementsByTagName("a");
+
+for (i = 1; i <= links.length; i++) {
+
+    console.log("This is a link number" + i);
+}
+
+document.write("All links are looped");
